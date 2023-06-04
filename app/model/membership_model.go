c4a6ce9691cd5db343f800f964d050c312849567
Devi Amalia <devifildza@gmail.com>
2023-06-04 19:19:21 +0700

    rev

--- app/model/membership_model.go
+++ app/model/membership_model.go
@@ -1,15 +1,27 @@
 package model
 
-import "time"
+import (
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Membership struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Phone     int       `json:"phone"`
-	BirthDay  time.Time `json:"birth_day"`
-	Level     string    `json:"level"`
-	Point     int       `json:"point"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID         int        `json:"id"`
+	MemberCode string     `json:"member_code"`
+	Name       string     `json:"name"`
+	Email      string     `json:"email"`
+	Phone      int        `json:"phone"`
+	BirthDay   string     `json:"birth_day"`
+	Level      string     `json:"level"`
+	Point      int        `json:"points"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  *time.Time `json:"updated_at"`
+}
+
+func (m *Membership) AfterFind(tx *gorm.DB) (err error) {
+	m.BirthDay = strings.Split(m.BirthDay, "T")[0]
+
+	return
 }
